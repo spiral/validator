9df05ba77b3b0e0b81488773e1366c460fd32fc7
Sergey Syomkin <sergey.syomkin@bestchange.dev>
2025-10-21 19:22:34 +0300

    Add ru locale translate

---
+++ locale/ru/spiral-validator-checker-passwordchecker.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Password must contain at least {1} uppercase letter.' => 'Пароль должен содержать как минимум {1} заглавную букву.',
+    'Password must contain at least {1} lowercase letter.' => 'Пароль должен содержать как минимум {1} строчную букву.',
+    'Password must contain at least {1} number.' => 'Пароль должен содержать как минимум {1} цифру.',
+    'Password must contain at least {1} special character.' => 'Пароль должен содержать как минимум {1} специальный символ.',
+];
