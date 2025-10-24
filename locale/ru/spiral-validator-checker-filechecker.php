a6d2b7aae73c1f0110c2afffa33d988c541098ec
Alexander Strizhak <gam6itko@gmail.com>
2025-10-24 11:54:54 +0300

    fix: Message trans fix (#24)
    * KB != 1024B
    
    * KiB translation fix

--- locale/ru/spiral-validator-checker-filechecker.php
+++ locale/ru/spiral-validator-checker-filechecker.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'File does not exists.' => 'Файл не найден.',
     'File not received, please try again.' => 'Не удалось получить файл, попробуйте еще раз.',
-    'File exceeds the maximum file size of {1}KB.' => 'Файл превышает допустимый размер {1}KB.',
+    'File exceeds the maximum file size of {1}KiB.' => 'Файл превышает допустимый размер {1}KiB.',
     'File has an invalid file format.' => 'Неверное расширение файла.',
 ];
